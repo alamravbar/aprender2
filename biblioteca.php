ec1e41c3bb897b48d37e34d09c2c5e8b3684244b
alamravbar <alam.ravbar@gmail.com>
2017-10-22 17:58:01 -0300

    Se realizo sumate y la inclusión de la biblioteca - y sumate

---
+++ biblioteca.php
@@ -0,0 +1,21 @@
+
+  <div class="botones">
+    <a href="#"><img src="img/iconos/+.png" alt="Subir Documento"></a>
+  </div>
+  <div class="container">
+    <p>
+    Biblioteca es una colección de los libros, documentos, videos, infografías realizadas por
+    nuestros docentes, no dude en usarlos!
+    </p>
+  </div>
+    <div class="show_archive" style="overflow-y: scroll; height:400px;">
+
+    </div>
+    <br>
+    <script type="text/javascript" src="js/jquery.js"></script>
+    <script type="text/javascript" src="js/bootstrap.js"></script>
+    <script type="text/javascript">
+    $.get("vista-archivos.php", function(data){
+      $(".show_archive").html(data);
+    });
+    </script>
---
+++ eliminar_archivo.php
@@ -0,0 +1,5 @@
+<?php
+if($_GET){
+  echo "Elimino el archivo.".$_GET['id'];
+}
+ ?>
