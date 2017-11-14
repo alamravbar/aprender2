06486559a80b6e7294859c77ae556ee6adbda8bf
marcelaMuller <marceasdfg@gmail.com>
2017-11-14 11:40:07 -0300

    mod de login

--- biblioteca-vista-archivos.php
+++ biblioteca-vista-archivos.php
@@ -1,10 +1,10 @@
 <?php
 include_once 'lib/PDOConfig.php';
-?>
-
-
-<?php
+include_once "lib/Login.php";
 $base = new PDOConfig();
+$oLogin=new Login();
+
+
 //id_documento 	nombre 	ruta 	extension 	descripcion 	id_categoria
 $sql=" select * from Documento";
 $mostrar = "";
@@ -33,8 +33,12 @@ if(!$resultado){
 
   $mostrar.="<div class='col-md-12 table-responsive'>";
   $mostrar.="<table class='table'>";
-  $mostrar.="<tr><thead style='text-align:center;'><td>Archivo</td><td>Funciones</td></thead></tr>";
-  $mostrar.="<tbody>";
+  $mostrar.="<tr><thead style='text-align:center;'><td>Archivo</td>";
+  if($oLogin->getRol()!=1){
+$mostrar.="<td>Funciones</td>";
+  }
+  
+  $mostrar.="</thead></tr><tbody>";
   foreach($datos as $elem){
 
       $mostrar.="<tr>";
@@ -45,14 +49,16 @@ if(!$resultado){
       $mostrar.="<td>";
       $mostrar.=" <a href='".$elem['ruta']."' class='list-group-item'><img src='img/".$extension.".jpg' class='img-rounded' align='center' width='68' height='68'> ".$elem['nombre']."</a>";
       $mostrar.="</td>";
+if($oLogin->getRol()!=1){
       $mostrar.="<td class='botones'>";
 
-      $mostrar.="<a href='biblioteca-eliminar_archivo.php?id=".$elem['id_documento']."'><img src='img/iconos/-.png' alt='Eliminar Archivo'></a>";
+      $mostrar.="<a href='eliminar.php?id=".$elem['id_documento']."'><img src='img/iconos/-.png' alt='Eliminar Archivo'></a>";
       $mostrar.="<a id='modificar_archivo' href='formulariomodificar.php?id=".$elem['id_documento']."'><img src='img/iconos/lapiz.png' alt='Eliminar Archivo'></a>";
 
       $mostrar.="<a id='validar_moderador' href='#'><img src='img/iconos/validar_v.png' alt='Eliminar Archivo'></a>";
       $mostrar.="<a id='validar_docente' href='#'><img src='img/iconos/validar_g.png' alt='Eliminar Archivo'></a>";
       $mostrar.="</td>";
+}
       $mostrar.="</tr>";
   }
   $mostrar.="</tbody>";
